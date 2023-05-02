fcd64a3d5db3abe984d26808a076b063fa18cbed
Melque Lima <melque_ex@yahoo.com.br>
2023-05-02 11:30:05 -0300

    update

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ classifiers = [
  
 setup(
   name='SqlUts',
-  version='0.1.1',
+  version='0.1.2',
   description='Date package',
   long_description=open('README.md').read() + '\n\n' + open('CHANGELOG.txt').read(),
   long_description_content_type='text/markdown',
